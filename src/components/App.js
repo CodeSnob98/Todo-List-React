bcbaf34d2cdc4dadcb484434c3e2e9c89c2b0c1a
CodeSnob98 <63289568+CodeSnob98@users.noreply.github.com>
2020-11-10 02:43:57 +0530

    change1

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,37 @@
 import React from "react";
 import "./../styles/App.css";
+import SubmitForm from "./submitForm";
+import ToDo from "./toDo";
+function App() {
+  const tasks = [];
+  const [task, setTask] = React.useState(tasks);
 
-function App() 
-{
-	return (
-	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
-	</div>
-	);
+  const handleDelete = (index) => {
+    const arrCopy = [...task];
+    arrCopy.splice(index, 1);
+    //console.log(arrCopy);
+    setTask(arrCopy);
+  };
+  const handleSubmit = (currTask) => {
+    if (currTask !== "") {
+      const arrCopy = [...task];
+      arrCopy.push(currTask);
+      console.log(currTask);
+      setTask(arrCopy);
+    }
+  };
+  const handleSave = (currTask, index) => {
+    let arrCopy = [...task];
+    arrCopy.splice(index, 1, currTask);
+    setTask(arrCopy);
+  };
+  return (
+    <div id="main">
+      <SubmitForm onSave={handleSubmit} />
+      <hr />
+      <ToDo tasks={task} onDelete={handleDelete} onSave={handleSave} />
+    </div>
+  );
 }
-
 
 export default App;
